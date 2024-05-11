62a99200104eff711515a6a6378ff2414b23bcad
Vaibhav Satyawan Mane <119654155+vaibhavsmane36@users.noreply.github.com>
2024-05-11 11:29:15 +0530

    Add files via upload

---
+++ PrimeCheck.java
@@ -0,0 +1,21 @@
+// Java program to check if a number is prime
+public class PrimeCheck {
+    public static boolean isPrime(int number) {
+        if (number <= 1)
+            return false;
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int number = 17;
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+}
