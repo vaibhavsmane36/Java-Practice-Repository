6ec73f74639788f18605b9e1ab74ac36b58bae18
Vaibhav Satyawan Mane <119654155+vaibhavsmane36@users.noreply.github.com>
2024-05-11 11:27:39 +0530

    Add files via upload

---
+++ SumOfTwoNumbers.java
@@ -0,0 +1,9 @@
+// Java program to find the sum of two numbers
+public class SumOfTwoNumbers {
+    public static void main(String[] args) {
+        int firstNumber = 5;
+        int secondNumber = 10;
+        int sum = firstNumber + secondNumber;
+        System.out.println("Sum: " + sum);
+    }
+}
