669ece354bfd359081060ca14787ef8f4ac2e7e5
Vaibhav Satyawan Mane <119654155+vaibhavsmane36@users.noreply.github.com>
2024-05-20 19:44:39 +0530

    Add files via upload

---
+++ BitwiseOperators.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class BitwiseOperators {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = input.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = input.nextInt();
+
+        System.out.println("Bitwise AND: " + (num1 & num2));
+        System.out.println("Bitwise OR: " + (num1 | num2));
+        System.out.println("Bitwise XOR: " + (num1 ^ num2));
+        System.out.println("Bitwise NOT: " + (~num1));
+        System.out.println("Bitwise Left Shift: " + (num1 << 2));
+        System.out.println("Bitwise Right Shift: " + (num1 >> 2));
+        System.out.println("Bitwise Unsigned Right Shift: " + (num1 >>> 2));
+
+        input.close();
+    }
+}
